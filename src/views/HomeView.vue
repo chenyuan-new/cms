a9a0357bd87056976b0795686a821d90bc17cad9
chen yuan <cy911110@qq.com>
2022-03-21 22:02:16 +0800

    chore: 格式化

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  name: 'HomeView',
+  setup() {
+    return {
+      defineComponent,
+      HelloWorld,
+    };
   },
 });
 </script>
